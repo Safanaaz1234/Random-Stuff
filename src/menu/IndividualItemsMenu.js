bb997c22da2cd351f09365da3416593954aac864
Safa <shaikhsohanaaz@gmail.com>
2023-06-14 20:33:58 +0530

    images updated

--- src/menu/IndividualItemsMenu.js
+++ src/menu/IndividualItemsMenu.js
@@ -5,6 +5,17 @@ import butterscotchMilkshake from "../images/butterscotchMilkshake.png";
 import blueLagoon from "../images/blueLagoon.png";
 import sandwich from "../images/sandwich.png";
 import chinesePasta from "../images/chinesePasta.png";
+import espresso from "../images/espresso.png";
+import cappuccino from "../images/cappuccino.png";
+import chocoEspresso from "../images/chocoEspresso.png";
+import americano from "../images/americano.png";
+import pastry from "../images/pastry.png";
+import chocoPastry from "../images/chocoPastry.png";
+import macaroons from "../images/macaroons.png";
+import pancake from "../images/pancake.png";
+import cookie from "../images/cookie.png";
+import croissant from "../images/croissant.png";
+import chocoCroissant from "../images/chocoCroissant.png";
 const IndividualItemsMenu = [
   {
     category: "tea-latte",
@@ -26,27 +37,27 @@ const IndividualItemsMenu = [
   },
   {
     category: "espresso",
-    name: "Chai Tea",
-    image: tea,
-    alt: "tea",
+    name: "Espresso",
+    image: espresso,
+    alt: "espresso",
   },
   {
     category: "chocolate-espresso",
-    name: "Chai Tea",
-    image: tea,
-    alt: "tea",
+    name: "Chocolate Espresso",
+    image: chocoEspresso,
+    alt: "chocolate-espresso",
   },
   {
     category: "cappuccino",
-    name: "Chai Tea",
-    image: tea,
-    alt: "tea",
+    name: "Cappuccino",
+    image: cappuccino,
+    alt: "cappuccino",
   },
   {
     category: "americano",
-    name: "Chai Tea",
-    image: tea,
-    alt: "tea",
+    name: "Americano",
+    image: americano,
+    alt: "americano",
   },
   {
     category: "butterscotch-milkshake",
@@ -62,45 +73,45 @@ const IndividualItemsMenu = [
   },
   {
     category: "strawberry-delight",
-    name: "Chai Tea",
-    image: tea,
-    alt: "tea",
+    name: "Strawberry Delight",
+    image: pastry,
+    alt: "strawberry-delight",
   },
   {
     category: "chocolate-pastry",
-    name: "Chai Tea",
-    image: tea,
-    alt: "tea",
+    name: "Chocolate Pastry",
+    image: chocoPastry,
+    alt: "chocolate-pastry",
   },
   {
     category: "macaroons",
-    name: "Chai Tea",
-    image: tea,
-    alt: "tea",
+    name: "Macaroons",
+    image: macaroons,
+    alt: "macaroons",
   },
   {
     category: "golden-honey-pancakes",
-    name: "Chai Tea",
-    image: tea,
-    alt: "tea",
+    name: "Golden Honey Pancake",
+    image: pancake,
+    alt: "golden-honey-pancakes",
   },
   {
     category: "chocochip-cookies",
-    name: "Chai Tea",
-    image: tea,
-    alt: "tea",
+    name: "Chocochip Cookies",
+    image: cookie,
+    alt: "chocochip-cookies",
   },
   {
     category: "butter-bliss",
-    name: "Chai Tea",
-    image: tea,
-    alt: "tea",
+    name: "Butter Bliss",
+    image: croissant,
+    alt: "butter-bliss",
   },
   {
     category: "chocolate-croissant",
-    name: "Chai Tea",
-    image: tea,
-    alt: "tea",
+    name: "Chocolate Croissant",
+    image: chocoCroissant,
+    alt: "chocolate-croissant",
   },
   {
     category: "grilled-cheese-sandwich",
